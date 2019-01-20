fb81b7f9d17113bdb5a093c23c99cf982025400f
François Lefebvre <francois.lefebvre@savoirfairelinux.com>
2019-01-20 16:11:08 -0500

    Config so far

---
+++ templates/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((pony-settings . (make-pony-project
+                         :python "/home/flefebvre/.virtualenvs/osm/bin/python"
+                         :pythonpath "/home/flefebvre/.virtualenvs/osm/lib/python2.7"
+                         :settings "settings.local"
+                         :appsdir "/home/flefebvre/projets/optel/osm/")))))
