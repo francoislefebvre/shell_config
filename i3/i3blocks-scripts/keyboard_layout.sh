497a2eb80de68d6ef316f72d1d5a9865b02313e0
François Lefebvre <francois.lefebvre@savoirfairelinux.com>
2019-01-25 22:25:49 -0500

    Change i3blocks colors

--- i3/i3blocks-scripts/keyboard_layout.sh
+++ i3/i3blocks-scripts/keyboard_layout.sh
@@ -4,7 +4,7 @@ CURRENT_LAYOUT=`setxkbmap -query | gawk '/layout:/ {print $2}'`
 echo $CURRENT_LAYOUT
 echo $CURRENT_LAYOUT
 
-[ $CURRENT_LAYOUT = "us" ] && echo "#CC6666" && exit 0
+[ $CURRENT_LAYOUT = "us" ] && echo "#B294BB" && exit 0
 [ $CURRENT_LAYOUT = "ca" ] && echo "#81A2BE"
 exit 0
 
